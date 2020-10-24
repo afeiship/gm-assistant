91d7628adaa5da28435956d21cae240a51bc1a73
afeiship <1290657123@qq.com>
2020-10-24 13:05:04 +0800

    fix: when disabled do not click

--- dist/index.js
+++ dist/index.js
@@ -2,9 +2,9 @@
  *  name: @feizheng/gm-assistant
  *  description: Tampermonkey assistant.
  *  homepage: https://github.com/afeiship/gm-assistant
- *  version: 1.0.0
- *  date: 2020-10-17T08:04:16.121Z
+ *  version: 1.0.1
+ *  date: 2020-10-24T05:04:54.928Z
  *  license: MIT
  */
 
-!function(t){"function"==typeof define&&define.amd?define(t):t()}((function(){$(document).ready(()=>{var t=$(".weui-msg"),e=t.find(".weui-msg__desc").text().trim();t.after('<button id="goto" class="weui-btn weui-btn_primary" style="cursor: pointer;">一键前往</button>'),$("#goto").click(()=>{location.href=e})})}));
+!function(e){"function"==typeof define&&define.amd?define(e):e()}((function(){$(document).ready(()=>{var e=$(".weui-msg"),t=e.find(".weui-msg__desc").text().trim(),i=e.find(".weui-msg__title").text().includes("已停止访问该网页")?"disabled":"primary";e.after(`<button id="goto" ${i} class="weui-btn weui-btn_${i}" style="cursor: pointer;">一键前往</button>`),$("#goto").click(()=>{location.href=t})})}));
--- src/plugins/weixin110.js
+++ src/plugins/weixin110.js
@@ -3,12 +3,19 @@
  * 微信屏蔽的网址，方便直接打开。
  */
 
+// https://detail.m.tmall.com/item.htm?id=560601547829&spm=a2141.7631571.designer_23244302224._1_0
+// https://www.xb5.cc/ztlist/Hban.html
+
 $(document).ready(() => {
   var target = $('.weui-msg');
   var target_url = target.find('.weui-msg__desc').text().trim();
-  target.after(`<button id="goto" class="weui-btn weui-btn_primary" style="cursor: pointer;">一键前往</button>`);
+  var title = target.find('.weui-msg__title');
+  var disabled = title.text().includes('已停止访问该网页') ? 'disabled' : 'primary';
+  target.after(
+    `<button id="goto" ${disabled} class="weui-btn weui-btn_${disabled}" style="cursor: pointer;">一键前往</button>`
+  );
 
-  $('#goto').click(()=>{
+  $('#goto').click(() => {
     location.href = target_url;
-  })
+  });
 });
