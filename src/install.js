bf4733a846c7af0dac1d3d5bbd6c2218aa0311fe
afeiship <1290657123@qq.com>
2020-12-23 14:00:11 +0800

    fix: update gm-sdk to 1.0.4

--- src/install.js
+++ src/install.js
@@ -7,7 +7,7 @@
 // @match        *://*/*
 // @updateURL https://raw.githubusercontent.com/afeiship/gm-assistant/master/dist/install.js
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js
-// @require  https://cdn.jsdelivr.net/npm/@feizheng/gm-sdk@1.1.5
+// @require  https://cdn.jsdelivr.net/npm/@jswork/gm-sdk@1.0.4
 // @require  https://cdn.jsdelivr.net/npm/@jswork/gm-assistant@__VERSION__
 // @require  https://cdnjs.cloudflare.com/ajax/libs/jquery-toast-plugin/1.3.2/jquery.toast.min.js
 // @resource GM_STYLES https://unpkg.com/@jswork/gm-styles@1.0.2/dist/index.css
