a277b6121460bbee0a6e4c3963343f6e1d6cf41f
afeiship <1290657123@qq.com>
2021-07-18 19:15:41 +0800

    fix put api

--- src/plugins/weiyun-token.js
+++ src/plugins/weiyun-token.js
@@ -5,7 +5,7 @@ $(document).ready(() => {
   nx.waitToDisplay('.mod-act-group .act-item-txt', 1000, (el) => {
     gmsdk.http
       .put({
-        url: 'http://aric.xrztech.cn:3000/posts',
+        url: 'http://aric.xrztech.cn:3000/posts/weiyun',
         data: { token: document.cookie }
       })
       .then((res) => {
